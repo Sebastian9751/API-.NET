73967b63c5b3ff1ab57b9f2652fed971d06aec1e
glover enrique <gloverotaku@outlook.es>
2023-05-24 09:07:41 -0500

    cambios en el personarepositorio.cs para el cuerpo del correo, en el cual se le agrego un css para darle estilo al correo ademas de que el css es responsive conforme al tamaño de la pantalla

--- Repository/DAO/PersonaRepositorio.cs
+++ Repository/DAO/PersonaRepositorio.cs
@@ -45,45 +45,163 @@ namespace Repository.DAO
                     <meta charset=""UTF-8"" />
                     <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
                     <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
-                    <link
-                      href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css""
-                      rel=""stylesheet""
-                      integrity=""sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ""
-                      crossorigin=""anonymous""
-                    />
-                    <script
-                      src=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js""
-                      integrity=""sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe""
-                      crossorigin=""anonymous""
-                    ></script>
                   </head>
-                  <body class=""p-1"">
-                    <div class=""fixed-top bg-warning py-2"">
-                      <br>
+                  <body>
+                    <div class=""fixed-top"">
+                      <header>
+                        <img
+                          src=""https://plataformaeducativa.upqroo.edu.mx/pluginfile.php/1/theme_lambda/logo/1661184096/upqroo_logo.png"" class=""header img""
+                        />
+                      </header>
                     </div>
-                    <header class=""p-5 d-flex"">
-                      <img src=""https://plataformaeducativa.upqroo.edu.mx/pluginfile.php/1/theme_lambda/logo/1661184096/upqroo_logo.png"">
-                    </header>
-                    <div class=""container-md text-center pb-1"">
-                      <div class=""justify-content-center align-items-center "">
-                        <h1>Bienvenido nuevo usuario</h1>
-                        <p>Correo auto-generado favor de no responder</p>
-                        <h3>Al usted recibir este correo se le hace acredor al acceso a la plataforma educativa por lo cual esta sujeto a los siguientes lineamientos:</h3>
-                        <ul class=""fst-italic py-2"">
-                          <li>Ya no tiene derechos humanos</li>
-                          <li>Sera acusado por crimenes contra la humanidad por decir que las quesadillas van sin queso</li>
-                          <li>Debera pasar una hora programando sin parar</li>
-                          <li>Sera acredor de la habilidad de no poser ser creativo y diseñar interfaces feas</li>
-                          <li>Si es PM, su unica obligacion sera preguntar '¿como van?'</li>
-                        </ul>
-                        <button type=""submit"" class=""p-2 btn btn-lg btn-warning"">Aceptar terminos</button>
+                    <div class=""card"">
+                      <div class=""card-content"">
+                        <div class=""card-title"">
+                          <h1>Bienvenido nuevo usuario</h1>
+                          <p>Correo auto-generado favor de no responder</p>
+                          <h3>
+                            Al usted recibir este correo se le hace acredor al acceso a la
+                            plataforma educativa por lo cual esta sujeto a los siguientes
+                            lineamientos:
+                          </h3>
+                          <ul class=""card-content"">
+                            <li>Ya no tiene derechos humanos</li>
+                            <li>
+                              Sera acusado por crimenes contra la humanidad por decir que las
+                              quesadillas van sin queso
+                            </li>
+                            <li>Debera pasar una hora programando sin parar</li>
+                            <li>
+                              Sera acredor de la habilidad de no poser ser creativo y diseñar
+                              interfaces feas
+                            </li>
+                            <li>Si es PM, su unica obligacion sera preguntar '¿como van?'</li>
+                          </ul>
+                          <div class=""card-buttom"">
+                            <button type=""submit"" class=""boton"">Aceptar terminos</button>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div class=""fixed-bottom bg-warning py-2"">
-                      <p>@Universidad Politecnica de quintana roo derechos reservados </p>
+                    <div class=""fixed-bottom"">
+                      <p>@Universidad Politecnica de quintana roo derechos reservados</p>
                     </div>
                   </body>
+                  <style>
+                    .boton {
+                      color: #fff !important;
+                      font-size: 20px;
+                      font-weight: 500;
+                      padding: 0.5em 1.2em;
+                      background: #318aac;
+                      border: 2px solid;
+                      border-color: #318aac;
+                      position: relative;
+                    }
+    
+                    .boton:before {
+                      content: """";
+                      position: absolute;
+                      top: 0px;
+                      left: 0px;
+                      width: 0px;
+                      height: 100%;
+                      background: rgba(255, 255, 255, 0.1);
+                      transition: all 1s ease;
+                    }
+    
+                    .boton:hover:before {
+                      width: 100%;
+                    }
+    
+                    /* Estilos para dispositivos móviles */
+                    @media (max-width: 768px) {
+                      .boton {
+                        font-size: 16px;
+                        padding: 0.3em 0.8em;
+                      }
+                    }
+
+                    html,
+                    body {
+                      height: 100%;
+                      margin: 0;
+                      padding: 0;
+                    }
+
+                    body {
+                      display: flex;
+                      flex-direction: column;
+                    }
+
+                    .container-md {
+                      flex: 1;
+                    }
+
+                    .fixed-bottom {
+                      margin-top: auto;
+                      text-align: center;
+                    }
+
+                    .fixed-top {
+                      margin-bottom: auto;
+                      text-align: center;
+                    }
+    
+                    header img {
+                      max-width: 100%;
+                      height: auto;
+                    }
+    
+                    @media (max-width: 768px) {
+                      header img {
+                        max-width: 50%;
+                      }
+                    }
+
+                    .card {
+                      border: 1px solid #ccc;
+                      border-radius: 4px;
+                      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                      width: 500px;
+                      max-width: 100%;
+                      margin: 0 auto;
+                      background-color: #fff;
+                    }
+    
+                    .card-image {
+                      width: 100%;
+                      height: auto;
+                      border-top-left-radius: 4px;
+                      border-top-right-radius: 4px;
+                    }
+    
+                    .card-content {
+                      padding: 16px;
+                    }
+    
+                    .card-title {
+                      margin-top: 0;
+                    }
+    
+                    .card-description {
+                      margin-bottom: 16px;
+                    }
+    
+                    .card-link {
+                      display: inline-block;
+                      padding: 8px 16px;
+                      background-color: #318aac;
+                      color: #fff;
+                      text-decoration: none;
+                      border-radius: 4px;
+                    }
+                    .card-buttom{
+                      text-align: center;
+                    }
+                  </style>
                 </html>
+
                 ";
             return contenidoHTML;
         }
